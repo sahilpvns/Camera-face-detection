25180f4667412f41005a457588c2279b9544cd7c
Sahil Prajapati <135958905+sahil-supplyop@users.noreply.github.com>
2023-10-17 04:04:19 +0530

    Camera Face Detection application

---
+++ app/src/main/java/com/hurrey/camerafacedetetion/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.hurrey.camerafacedetetion
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.hurrey.camerafacedetetion.camerax.CameraManager
+import com.hurrey.camerafacedetetion.camerax.GraphicOverlay
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var cameraManager: CameraManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        createCameraManager()
+        checkForPermission()
+        onClicks()
+
+    }
+
+    private fun checkForPermission() {
+        if (allPermissionsGranted()) {
+            cameraManager.startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun onClicks() {
+        val btnSwitch = findViewById<Button>(R.id.btnSwitch)
+        btnSwitch.setOnClickListener {
+            cameraManager.changeCameraSelector()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                cameraManager.startCamera()
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                    .show()
+                finish()
+            }
+        }
+    }
+
+    private fun createCameraManager() {
+        val previewViewFinder = findViewById<PreviewView>(R.id.previewView_finder)
+        val graphicOverlayFinder = findViewById<GraphicOverlay>(R.id.graphicOverlay_finder)
+        cameraManager = CameraManager(this, previewViewFinder, this, graphicOverlayFinder)
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(android.Manifest.permission.CAMERA)
+    }
+
+}
